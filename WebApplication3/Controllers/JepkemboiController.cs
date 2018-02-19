2aa4a2498391b55ae81a8648f4394740b7a66fae
Monjep <35704694+Monjep@users.noreply.github.com>
2018-02-19 15:13:22 -0600

    assigment4

---
+++ WebApplication3/Controllers/JepkemboiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class JepkemboiController : Controller
+    {
+        // GET: Jepkemboi
+        [Route("Jepkemboi")]
+        [Route("Jepkemboi/Index")]
+        public ActionResult Index()
+        {
+            ViewData["message"] = "Index.cshtml";
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
